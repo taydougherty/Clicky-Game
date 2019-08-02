37637e19073a4a97e06df5b46bdac9c55617b1f0
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-08-01 20:21:41 -0700

    updated images and formatted folders

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import PuppyCard from "./components/PuppyCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import puppies from "./puppies.json";
+
+class App extends Component {
+  // Setting this.state.puppies to the puppies json array
+  state = {
+    puppies: sortfunction,
+    counter: 0,
+    ifClicked =[]
+  };
+
+  // rename to when clicked image
+  removePuppy = id => {
+    // conditionals:
+    // if already in the ifClicked array:
+    // set state of counter to 0
+    // alert "already been clicked"
+    // empty ifClicked array
+    // else:
+    // add count +1 to counter
+    // reshuffle using sort function
+    // when hit 12:
+    // counter = 0
+    // alert "you win"
+    // reshuffle using sort function
+
+    // Filter this.state.puppies for puppies with an id not equal to the id being removed
+    const puppies = this.state.puppies.filter(friend => friend.id !== id);
+    // Set this.state.puppies equal to the new puppies array
+    this.setState({ puppies: sortfunction(google it) });
+  };
+
+  // Map over this.state.puppies and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>puppies List</Title>
+        {this.state.puppies.map(Puppy => (
+          <PuppyCard
+            removePuppy={this.removePuppy}
+            id={Puppy.id}
+            key={Puppy.id}
+            name={Puppy.name}
+            image={Puppy.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
--- src/components/PuppyCard/index.js
+++ src/components/PuppyCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function PuppyCard(props) {
   return (
     <div className="card">
       <div className="img-container">
@@ -20,11 +20,11 @@ function FriendCard(props) {
           </li>
         </ul>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
+      <span onClick={() => props.removePuppy(props.id)} className="remove">
         𝘅
       </span>
     </div>
   );
 }
 
-export default FriendCard;
+export default PuppyCard;
